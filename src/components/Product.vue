0ac1881b81fcdd5651b7117e38ca6e1cd7e27712
Zzzul <mohammad.zulfahmi03@gmail.com>
2023-09-18 16:55:37 +0700

    add filter product

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -2,10 +2,34 @@
 import products from '../assets/data/products.json'
 import categories from '../assets/data/categories.json'
 import { useCartStore } from '../stores/cart'
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 const cart = useCartStore()
 const search = ref('')
+
+const filteredProducts = ref(products)
+const selectedCategory = ref(0)
+
+const filterCategory = (id) => {
+    if (id != 0) {
+        if (id == selectedCategory.value) {
+            selectedCategory.value = 0
+            filteredProducts.value = products
+            return
+        }
+
+        filteredProducts.value = products.filter(product => product.category_id == id)
+        selectedCategory.value = id
+        return
+    }
+
+    filteredProducts.value = products
+    selectedCategory.value = 0
+}
+
+watch(search, () => {
+    filteredProducts.value = products.filter(product => product.name.toLowerCase().includes(search.value.toLowerCase()))
+})
 </script>
 
 <template>
@@ -13,8 +37,10 @@ const search = ref('')
         <div class="row row-cols-1 row-cols-md-3 g-4">
             <div class="col-md-12">
                 <div class="row g-0">
-                    <div class="col-md-2 mb-3" v-for="(category, i) in categories" :key="category.id">
-                        <div class="card" :class="{ 'ms-3': i != 0 }" style="cursor: pointer;">
+                    <div class="col-md-2 mb-3" v-for="(category, i) in categories" :key="category.id"
+                        @click="filterCategory(category.id)">
+                        <div class="card" :class="{ 'ms-3': i != 0, 'border-primary': selectedCategory == category.id }"
+                            style="cursor: pointer;">
                             <div class="card-body p-2">
                                 <div class="row">
                                     <div class="col-md-4">
@@ -29,8 +55,8 @@ const search = ref('')
                         </div>
                     </div>
 
-                    <div class="col-md-2 mb-3">
-                        <div class="card ms-3">
+                    <div class="col-md-2 mb-3" @click="filterCategory(0)" style="cursor: pointer;">
+                        <div class="card ms-3" :class="{ 'border-primary': selectedCategory == 0 }">
                             <div class="card-body p-2">
                                 <div class="row">
                                     <div class="col-md-4">
@@ -46,16 +72,17 @@ const search = ref('')
                     </div>
                 </div>
 
-                <input type="text" class="form-control" placeholder="Search..." aria-label="Search" v-model="search"
+                <input type="search" class="form-control" placeholder="Search..." aria-label="Search" v-model="search"
                     aria-describedby="inputGroup-sizing-default">
-
             </div>
-            <div class="col-md-2" v-for="product in products" :key="product.id" @click="cart.addToCart(product)">
+
+            <div class="col-md-2" v-for="product in filteredProducts" :key="product.id" @click="cart.addToCart(product)">
                 <div class="card" style="cursor: pointer;">
-                    <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                    <img src="https://placehold.co/600x400" class="card-img-top" :alt="product.name">
                     <div class="card-body">
                         <h5 class="card-title">{{ product.name }}</h5>
-                        <p class="card-text">{{ cart.formatCurrency(product.price) }}</p>
+                        <p class="card-text mb-1">{{ cart.formatCurrency(product.price) }}</p>
+                        <!-- <small class="fw-light">Stock: {{ product.stock }}</small> -->
                     </div>
                 </div>
             </div>
