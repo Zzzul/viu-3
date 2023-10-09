671e52c2c4909fc07d2a2c23ef94fedf462fb8b9
Zzzul <mohammad.zulfahmi03@gmail.com>
2023-10-09 10:30:21 +0700

    change alert to toast and add jsdoc

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import "vue-toastification/dist/index.css"
+
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import Toast from "vue-toastification"
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -11,8 +14,25 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const toastOptions = {
+    position: "top-center",
+    timeout: 2500,
+    closeOnClick: true,
+    pauseOnFocusLoss: true,
+    pauseOnHover: true,
+    draggable: true,
+    draggablePercent: 0.6,
+    showCloseButtonOnHover: false,
+    hideProgressBar: false,
+    closeButton: "button",
+    icon: true,
+    rtl: false,
+    transition: "Vue-Toastification__slideBlurred",
+    newestOnTop: true
+}
 
 app.use(createPinia().use(piniaPluginPersistedstate))
 app.use(router)
+app.use(Toast, toastOptions)
 
 app.mount('#app')
